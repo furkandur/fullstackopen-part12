374ceab9160bc9ab84c677923aafeaca8a5a6e31
furkandur <furkandursun3428@gmail.com>
2025-04-10 18:40:56 +0300

    Exercises 12.9. - 12.11.

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo');
 const router = express.Router();
+const { setAsync, getAsync } = require('../redis')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,11 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   })
+
+  const currentCount = await getAsync('added_todos')
+  const newCount = currentCount ? parseInt(currentCount) + 1 : 1
+  await setAsync('added_todos', newCount)
+
   res.send(todo);
 });
 
